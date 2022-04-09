a38240ea9f73fa729a989fed6f628178f4dd370e
osagie19 <osagieogiefo@gmail.com>
2022-04-08 18:01:55 -0700

    task 0

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,9 +9,10 @@ int main(void)
 {
 	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++);
 
-	putchar(letter); 
+	for(letter = 'a'; letter <= 'z'; letter++);
+
+	putchar(letter);
 
 
 	putchar('\n');
